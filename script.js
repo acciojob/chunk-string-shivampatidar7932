53aed4492070fbcd87e55f3b53f699dc849afa96
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-13 23:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function stringChop(str, size) {
-  // your code here
+//   // your code here
+	let ans=[];
+let i=0;
+while(i<=str.length-1){
+	ans.push(str.slice(i,i+size));
+	i=i+1;
 }
+return ans;
+	
+ }
 
 // Do not change the code below
 const str = prompt("Enter String.");
