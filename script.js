db009c1bf68e12a40db98362c85bb0866ee6228e
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-19 20:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ function stringChop(str, size) {
 	if(str === null) return [];
   let ans = [];
   let i = 0;
-
-  while (i <= str.length - 1) {
+while (i <= str.length - 1) {
   
     ans.push(str.slice(i, i + size));	  
     i = i + size;
   }
-
   return ans;
 }
 
 // Example of using the function with prompt
 const str = prompt("Enter String.");
+
 const size = parseInt(prompt("Enter Chunk Size.")); 
 // // Ensure size is treated as an integer
 const result = stringChop(str, size);
